eec9d8fa0ccf4e6a0b4d169b061e533f9143c6bd
jucianosilva153 <jucianodasilvasilvaa@gmail.com>
2023-06-27 23:59:36 +0100

    Adicionado suporte a CORS

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>  
+{  
+    options.AddDefaultPolicy(  
+        policy =>  
+        {  
+            policy.AllowAnyOrigin();  //set the allowed origin  
+        });  
+}); 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,7 +25,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
